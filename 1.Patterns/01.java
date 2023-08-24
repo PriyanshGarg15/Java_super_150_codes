ae1a0c5e8c147e8beee1fe48467291bdefe34594
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-08-24 21:12:25 +0530

    Create 01.java

---
+++ 1.Patterns/01.java
@@ -0,0 +1,22 @@
+package lecture_2;
+import java.util.*;
+public class pattern_1
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int row=1;
+        int star=n;
+        while(row<=n)
+        {
+            int i=1;
+            while(i<=star)
+            {
+                System.out.print("*");
+                i++;
+            }
+            row++;
+            System.out.println();
+        }
+    }
+}
